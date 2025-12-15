ebcbe1e8dde29e21b1e1cc92a8ee7b3fcf20fc93
ecocoo <sudoplatov.05@mail.ru>
2025-12-15 16:37:22 +0500

    fix

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,18 +1,4 @@
-const path = require("path");
-
-const buildNextEslintCommand = (filenames) =>
-  `yarn next:lint --fix --file ${filenames
-    .map((f) => path.relative(path.join("packages", "nextjs"), f))
-    .join(" --file ")}`;
-
-const checkTypesNextCommand = () => "yarn next:check-types";
-
-const buildHardhatEslintCommand = (filenames) =>
-  `yarn hardhat:lint-staged --fix ${filenames
-    .map((f) => path.relative(path.join("packages", "hardhat"), f))
-    .join(" ")}`;
-
 module.exports = {
-  "packages/nextjs/**/*.{ts,tsx}": "yarn eslint --fix",
-  "packages/hardhat/**/*.{ts,tsx}": "yarn eslint --fix",
+  "*.{js,jsx,ts,tsx}": ["prettier --write"],
+  "*.{json,md}": ["prettier --write"],
 };
